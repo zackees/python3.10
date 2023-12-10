06436835b5e61a984e7996a602088262e5535559
Zachary Vorhies <zachvorhies@protonmail.com>
2023-12-09 17:18:22 -0800

    Update winpyinstall310.bat

--- winpyinstall310.bat
+++ winpyinstall310.bat
@@ -7,5 +7,6 @@ powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'"
 
 echo Installing Python 3.10.10...
 %filename% /quiet PrependPath=1
+start /wait %filename% /quiet PrependPath=1
 
 echo Installation complete.
