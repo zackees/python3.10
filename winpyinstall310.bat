17cb6d00fa15d862be6df30b6a23ca374e28333f
Zachary Vorhies <zachvorhies@protonmail.com>
2023-12-09 17:06:50 -0800

    Update winpyinstall310.bat

--- winpyinstall310.bat
+++ winpyinstall310.bat
@@ -3,7 +3,7 @@ SET url=https://github.com/zackees/python3.10/raw/main/win/python-3.10.10-amd64.
 SET filename=python-3.10.10-amd64.exe
 
 echo Downloading Python 3.10.10...
-certutil -urlcache -split -f %url% %filename%
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%filename%'"
 
 echo Installing Python 3.10.10...
 start /wait %filename% /quiet InstallAllUsers=1 PrependPath=1
